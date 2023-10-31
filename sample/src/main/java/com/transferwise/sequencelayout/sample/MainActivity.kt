20f95c46d445879aa33ccad7105d1872b85911ac
Jayesh kukadiya <kukadia.jayesh@gmail.com>
2023-10-31 23:40:31 +0530

    Show progress from active to next step

--- sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
+++ sample/src/main/java/com/transferwise/sequencelayout/sample/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.transferwise.sequencelayout.sample
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mikashboks.sequencelayout.sample.R
+import com.transferwise.sequencelayout.SequenceLayout
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findViewById<SequenceLayout>(R.id.sequenceLayout).apply {
+            setProgressDotStepIndex(1)
+            setProgressDotStepMax(1000)
+            setProgressDotStepCurrent(300)
+
+            postDelayed({
+                setProgressDotStepCurrent(700)
+            }, 5000)
+        }
+    }
+}
